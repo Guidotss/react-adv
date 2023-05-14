b53d72f3d67563f7d6dff494e0dab96af83d6b46
Guidotss <guidoolguin@hotmail.com>
2023-05-14 15:01:18 -0300

    Rutas anidadas listas

---
+++ src/01-lazyload/layout/LazyLayout.tsx
@@ -0,0 +1,39 @@
+import { NavLink,Switch, Route } from "react-router-dom";
+import { LazyPage1, LazyPage2, LazyPage3 } from "../pages";
+
+
+export const LazyLayout = () => {
+  return (
+    <div>
+        <h1>LazyLayout Page</h1>
+        <ul>
+            <li>
+                <NavLink to="lazy1">
+                    Lazy-1
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="lazy2">
+                    Lazy-2
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="lazy3">
+                    Lazy-3
+                </NavLink>
+            </li>
+        </ul>
+
+        <Switch>
+            <Route path="*/lazy1" component={ LazyPage1 } />
+            <Route path="*/lazy2" component={LazyPage2} />
+            <Route path="*/lazy3" component={LazyPage3} />
+            <Route path="*" component={LazyPage1} />
+        </Switch>
+
+    </div>
+  )
+}
+
+
+export default LazyLayout;
